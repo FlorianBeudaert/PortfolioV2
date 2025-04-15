7a116be54ab2da8bef2bb33aaacdb9eb0c8b152e
Florian <florian.beudaert@epitech.eu>
2025-04-15 10:22:21 +0200

    prettier added and project section

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,43 +1,43 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
-import Navbar from "../layouts/Navbar";
-import Footer from "../layouts/Footer";
+import type { Metadata } from 'next';
+import { Geist, Geist_Mono } from 'next/font/google';
+import './globals.css';
+import Navbar from '../layouts/Navbar';
+import Footer from '../layouts/Footer';
 
 const geistSans = Geist({
-    variable: "--font-geist-sans",
-    subsets: ["latin"],
+  variable: '--font-geist-sans',
+  subsets: ['latin'],
 });
 
 const geistMono = Geist_Mono({
-    variable: "--font-geist-mono",
-    subsets: ["latin"],
+  variable: '--font-geist-mono',
+  subsets: ['latin'],
 });
 
 export const metadata: Metadata = {
-    title: "Florian Beudaert - Portfolio",
-    description: "Développeur Full Stack",
+  title: 'Florian Beudaert - Portfolio',
+  description: 'Développeur Full Stack',
 };
 
 export default function RootLayout({
-                                       children,
-                                   }: Readonly<{
-    children: React.ReactNode;
+  children,
+}: Readonly<{
+  children: React.ReactNode;
 }>) {
-    return (
-        <html lang="en">
-        <head>
-            <link rel="icon" href="/favicon.ico" />
-        </head>
-        <body
-            className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-        >
+  return (
+    <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.ico" />
+      </head>
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
         <Navbar />
         <div className="flex flex-col min-h-screen">
-            <main className="flex-1">{children}</main>
-            <Footer />
+          <main className="flex-1">{children}</main>
+          <Footer />
         </div>
-        </body>
-        </html>
-    );
+      </body>
+    </html>
+  );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
-import HeroSection from "@/components/HeroSection";
+import HeroSection from '@/pages/HeroSection';
+import ProjectsSection from '@/pages/Projects';
 
 export default function Home() {
   return (
     <main>
       <HeroSection />
+      <ProjectsSection />
     </main>
   );
 }
